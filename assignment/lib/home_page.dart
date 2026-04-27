031993e153b6382f42f8e0c4e7c29bb3f52aaabc
Musa220 <shalemohammad001@gmail.com>
2026-04-27 21:09:51 +0600

    2nd commit

--- assignment/lib/home_page.dart
+++ assignment/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,34 +13,61 @@ class HomePage extends StatelessWidget {
         foregroundColor: Colors.white,
         leading: Icon(Icons.dashboard, color: Colors.white),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              "Hello 63E",
-              style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                "Hello 63E",
+                style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 30),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-              "Welcome to our class",
-              style: TextStyle(color: Colors.green[700], fontSize: 20),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                "Welcome to our class",
+                style: TextStyle(color: Colors.green[700], fontSize: 20),
+              ),
             ),
-          ),
-          Container(
-            width: 300,
-            height: 400,
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(color: Colors.orangeAccent),
-            child: Text(
-              "I am container!!!",
-              style: TextStyle(color: Colors.white, fontSize: 22),
+            SizedBox(
+              height: 200,
+              width: 200,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                color: Colors.blueAccent,
+                child: Center(
+                  child: Image.asset(
+                    "assets/images/musa.jpeg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+            Image.network(
+              "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
+            ),
+            Container(
+              width: 300,
+              height: 400,
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(20),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.orangeAccent,
+                border: Border.all(color: Colors.black, width: 3),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                //shape: BoxShape.circle,
+              ),
+              child: Text(
+                "I am container!!!",
+                style: GoogleFonts.lobster(fontSize: 22),
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepPurple,
@@ -50,7 +78,7 @@ class HomePage extends StatelessWidget {
       endDrawer: NavigationDrawer(
         children: [
           ListTile(
-            leading: Icon(Icons.home_filled, color: Colors.black),
+            leading: Icon(Icons.home_filled, color: Colors.blue),
             title: Text("HomePage"),
             onTap: () {},
           ),
